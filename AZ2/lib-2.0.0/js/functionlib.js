59be99e4daabdbf021ece5ceb1f5f65d4b414806
Frank Udnæs <frankudnes601@msn.com>
2020-03-30 18:12:17 +0200

    dfhdfhdfh

--- AZ2/lib-2.0.0/js/functionlib.js
+++ AZ2/lib-2.0.0/js/functionlib.js
@@ -2957,3 +2957,17 @@ function setParallaxImages(ParallaxImages)
         _CurrentImage.css({ 'background-image': 'url(' + ImagesContent.url + ')', 'height': ImagesContent.height, 'opacity': ImagesContent.opacity });
     });
 }
+
+function AZGuid()
+{
+    Guid();
+}
+
+function Guid()
+{
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c)
+    {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
