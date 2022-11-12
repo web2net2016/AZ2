8c783e16d71a6353a8e49e3d54139b467aab49f5
Frank Udnæs <frankudnes601@msn.com>
2022-11-12 12:23:20 +0100

    gdgdfgdfg

---
+++ AZ2/lib-2/js/clientstorage.js
@@ -0,0 +1,182 @@
+﻿function clientStorage(ActionType, Name, Value)
+{
+    return AZClientStorage(ActionType, Name, Value);
+}
+
+function AZClientStorage(ActionType, Name, Value)
+{
+    var _ThisLocation = window.document.location.hostname;
+    var _LocalStorageEnabled = checkLocalStorage();
+    if (typeof Value === "object")
+    {
+        Value = JSON.stringify(Value);
+    }
+    if (_LocalStorageEnabled == true)
+    {
+        if (ActionType == "set")
+        {
+            setLocalStorage(_ThisLocation + "-" + Name, Value);
+        }
+        else if (ActionType == "get")
+        {
+            return parseData(getLocalStorage(_ThisLocation + "-" + Name));
+        }
+        else if (ActionType == "remove")
+        {
+            removeLocalStorage(_ThisLocation + "-" + Name);
+        }
+        else if (ActionType == "delete")
+        {
+            removeLocalStorage(_ThisLocation + "-" + Name);
+        }
+        else
+        {
+            clientStorageError("Local Storage: Wrong action type.");
+        }
+    }
+    else
+    {
+        var _CookieEnabled = checkCookie();
+        if (_CookieEnabled == true)
+        {
+            if (ActionType == "set")
+            {
+                setCookie(_ThisLocation + "-" + Name, Value);
+            }
+            else if (ActionType == "get")
+            {
+                return parseData(getCookie(_ThisLocation + "-" + Name));
+            }
+            else if (ActionType == "remove")
+            {
+                removeCookie(_ThisLocation + "-" + Name);
+            }
+            else if (ActionType == "delete")
+            {
+                removeCookie(_ThisLocation + "-" + Name);
+            }
+            else
+            {
+                clientStorageError("Cookies: Wrong action type.");
+            }
+        }
+        else
+        {
+            clientStorageError("Local Storage / Cookies not supported.");
+        }
+    }
+
+    function checkLocalStorage()
+    {
+        try
+        {
+            var _SupportsLocalStorage = !!window.localStorage && typeof localStorage.getItem === 'function' && typeof localStorage.setItem === 'function' && typeof localStorage.removeItem === 'function';
+            if (_SupportsLocalStorage)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        catch (e)
+        {
+            return false;
+        }
+    }
+
+    function setLocalStorage(LSName, LSValue)
+    {
+        localStorage.setItem(LSName, LSValue);
+    }
+
+    function getLocalStorage(LSName)
+    {
+        return localStorage.getItem(LSName);
+    }
+
+    function removeLocalStorage(LSName)
+    {
+        localStorage.removeItem(LSName);
+    }
+
+    function checkCookie()
+    {
+        try
+        {
+            if (navigator.cookieEnabled)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        catch (e)
+        {
+            return false;
+        }
+    }
+
+    function setCookie(CName, CValue)
+    {
+        var _Date = new Date();
+        _Date.setTime(_Date.getTime() + (365 * 24 * 60 * 60 * 1000));
+        var _Expires = "expires=" + _Date.toUTCString();
+        document.cookie = CName + "=" + CValue + "; " + _Expires;
+    }
+
+    function getCookie(CName)
+    {
+        var _Name = CName + "=";
+        var _DecodedCookie = decodeURIComponent(document.cookie);
+        var _CA = _DecodedCookie.split(';');
+        for (var i = 0; i < _CA.length; i++)
+        {
+            var _C = _CA[i];
+            while (_C.charAt(0) == ' ')
+            {
+                _C = _C.substring(1);
+            }
+            if (_C.indexOf(_Name) == 0)
+            {
+                return _C.substring(_Name.length, _C.length);
+            }
+        }
+        return "";
+    }
+
+    function removeCookie(CName)
+    {
+        document.cookie = CName + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    }
+
+    function clientStorageError(consoleText)
+    {
+        console.error("Client Storage Error\n" + consoleText);
+    }
+
+    function parseData(Data)
+    {
+        var _ReturnObj = "";
+        if (Data != null && Data != "")
+        {
+            var _Found = false;
+            if (Data.indexOf("{") == 0 || Data.indexOf("[") == 0)
+            {
+                _Found = true;
+            }
+            if (_Found == true)
+            {
+                _ReturnObj = $.parseJSON(Data);
+            }
+            else
+            {
+                _ReturnObj = Data;
+            }
+        }
+        return _ReturnObj;
+    }
+}
